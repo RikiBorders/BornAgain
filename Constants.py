e0b22c4b872776d490280b70e87a25f1c4e2b429
RikiBorders <ninjab02@hotmail.com>
2025-11-30 04:51:14 +0000

    rebase

--- Constants.py
+++ Constants.py
@@ -1,3 +1,12 @@
 REQUEST_HEADERS = {'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Mobile Safari/537.36'}
-GAMESPOT_COROUTINE_WAIT_TIME_IN_SECONDS = 86400  # 24 hours
-DEFAULT_ROLE_NAME = "Goon" #Default role name. Must exactly match the role name in Discord server.
+INTRO_TIMER_CHECK_TICK_IN_SECONDS = 1  # Keep this set to one second. This way, the timer is updated every second.
+AUTO_DISCONNECT_TICK_IN_SECONDS = 3  # Keep this set to one second. This way, the timer is updated every second.
+
+INTRO_FILE_PATH = 'media/audio/'
+USER_ID_TO_INTRO_FILE_MAP = {
+    937798657443512340: INTRO_FILE_PATH+'your_time_has_come_intro.wav',  # Nick
+    257557061448105996: INTRO_FILE_PATH+'charlie_hustle_intro.wav',  # Rik
+    257643785448718336: INTRO_FILE_PATH+'KOTH2_intro.wav',  # Conner
+    251134467924688901: INTRO_FILE_PATH+'lizard_intro.wav',  # Matty
+}
+DEFAULT_ROLE_NAME = "Goon" # This must be exactly the name of the appropriate role
